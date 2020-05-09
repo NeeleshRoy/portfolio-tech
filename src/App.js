f9141cca0551a9d5f572e79b75d3e460536f77ad
NeeleshRoy <neelesh.roy@bestbuy.com>
2020-05-09 20:35:14 +0530

    Added base navigation

--- src/App.js
+++ src/App.js
@@ -2,42 +2,18 @@
 import React from 'react'
 import { Nav } from './components'
 import { routes } from './components/routes'
-import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
-import { Container, Row } from 'react-bootstrap'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import './sass/App.scss'
 
 function App () {
   return (
     <Router>
-      <Container>
-        <div className="App">
-          <Row>
-            <header>
-              <div className="logo">
-                Logo
-              </div>
-              <Nav routes={routes} />
-            </header>
-          </Row>
-          <Switch>
-            {
-              routes.map(route => {
-                return route.name === 'Home'
-                  ? <Route
-                    key={route.path}
-                    path={route.path}
-                    component={route.component}
-                    exact
-                  />
-                  : <Route
-                    key={route.path}
-                    path={route.path}
-                    component={route.component}
-                  />
-              })
-            }
-          </Switch>
+      <div className="App">
+        <div className="container">
+          <Nav routes={routes} />
         </div>
-      </Container>
+      </div>
     </Router>
   )
 }
--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -1,24 +1,66 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
+import { Link, Switch, Route } from 'react-router-dom'
+import { initMenu } from '../../menu'
+import { Image } from 'react-bootstrap'
 
-export const Nav = (props) => {
-  const { routes } = props
-  return (
-    <nav>
-      <ul>
-        {
-          routes.map(route => {
-            return (
-              <li key={route.path}>
-                <Link to={`${route.path}`}>{route.name}</Link>
-              </li>
-            )
-          })
-        }
-      </ul>
-    </nav>
-  )
+export class Nav extends Component {
+  componentDidMount () {
+    initMenu()
+  }
+
+  render () {
+    const { routes } = this.props
+    return (
+      <div>
+        <div id="perspective" className="perspective effect-moveleft">
+          <div className="container-perspective">
+            <div className="wrapper">
+              <div className="main clearfix">
+                <header>
+                  <div className="logo">
+                    <Image src='/images/logo.svg' className="logo"></Image>
+                  </div>
+                  <button id="showMenu"><i className="fa fa-bars"></i></button>
+                </header>
+              </div>
+              <Switch>
+                {
+                  routes.map(route => {
+                    return route.name === 'Home'
+                      ? <Route
+                        key={route.path}
+                        path={route.path}
+                        component={route.component}
+                        exact
+                      />
+                      : <Route
+                        key={route.path}
+                        path={route.path}
+                        component={route.component}
+                      />
+                  })
+                }
+              </Switch>
+            </div>
+          </div>
+          <nav className="outer-nav right vertical">
+            <ul>
+              {
+                routes.map(route => {
+                  return (
+                    <li key={route.path}>
+                      <Link to={`${route.path}`}>{route.name}</Link>
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </nav>
+        </div>
+      </div>
+    )
+  }
 }
 
 Nav.propTypes = {
